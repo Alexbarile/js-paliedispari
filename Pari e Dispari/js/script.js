99022561e3045160b8f1cb8436a81b88737a8ddc
“Alexbarile” <alexbarile93fg@yahoo.it>
2022-12-05 17:34:29 +0100

    creata funzione somma num utente + num computer

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,2 +1,21 @@
 let pariDispari = prompt('Pari o Dispari');
-let numero = parseInt(prompt('Inserisci un numero da 1 a 5'));
+let numero_utente = parseInt(prompt('Inserisci un numero da 1 a 5'));
+console.log(numero_utente)
+
+function generateNumRandom(min, max){
+    let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumber;
+}
+
+let numero_computer = generateNumRandom(1, 5);
+
+console.log(numero_computer)
+
+function somma(numero_utente, numero_computer){
+    let sommaTot = numero_utente + numero_computer
+    console.log(sommaTot)
+    return sommaTot;
+
+}
+
+let sommaTot = somma(numero_utente, numero_computer);
